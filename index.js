54a7f6b886837201bb11e0273dbb4bd8046dc3a2
Ohkhalaf <Ohkhalaf@gmail.com>
2020-05-29 01:26:58 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const config = require("./config.json");
+const fs = require("fs");
 const discord = require("discord.js");
 var channel;
 const client = new discord.Client();
 
-const channelID = "Id for vent channel";
+var botInfo = {
+  Token: "",
+  channelID: "",
+};
 
 //sign in event
 client.on("ready", () => {
@@ -11,6 +16,37 @@ client.on("ready", () => {
 
 //when it receives a message
 client.on("message", (msg) => {
+  //if it's a message from the bot
+  if (msg.author.id == client.user.id) {
+    return; //just ignore it
+  }
+
+  if (
+    msg.channel.type == "text" && //if this is in a server
+    msg.member.permissions.has("ADMINISTRATOR") && //and the user is an admin
+    msg.content.startsWith("!setChannel") //and they use the command
+  ) {
+    msg.reply("channel has been set!");
+    botInfo.channelID = msg.channel.id; //this sets the channelID
+    var jsonString = JSON.stringify(botInfo);
+    fs.writeFile("./config.json", jsonString, function () {
+      console.log("channel Changed");
+    });
+    //this gets the channel object
+    client.channels.fetch(botInfo.channelID).then((result) => {
+      channel = result;
+      console.log(channel);
+    });
+    return;
+  }
+
+  if (msg.content == "!help") {
+    msg.reply(
+      "You dm me your message and I repeat it in the pre-decided vent channel, use !help to repeat this message"
+    );
+    return;
+  }
+
   //if it's in DMs
   if (msg.guild == null) {
     //send the message content to the channel
@@ -18,12 +54,15 @@ client.on("message", (msg) => {
   }
 });
 
+botInfo.Token = config.Token;
+botInfo.channelID = config.channelID;
+
 //signs in as the bot
-client.login("Token");
+client.login(botInfo.Token);
 //waits three seconds for sign in to finish and then gets the vent channel to post messages in
 setTimeout(function () {
-  client.channels.fetch(channelID).then((result) => {
+  client.channels.fetch(botInfo.channelID).then((result) => {
     channel = result;
-    console.log(channel);
+    console.log(botInfo.channelID);
   });
 }, 3000);
